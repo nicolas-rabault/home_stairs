39ca0b4e5be2afffd54c74a88a9d184de95233b8
Nicolas Rabault <nicolas.rabault@luos.io>
2022-01-26 15:34:53 +0100

    Add a script allowing to build and flash the entire product

---
+++ firmware/nodes/stair_step/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# build
+pio run -e l0_bootloader -e l0_master_bootloader
+
+# flash slave steps
+pyluos-bootloader flash -t 3 4 5 6 7 8 9 10 11 12 -b .pio/build/l0_bootloader/firmware.bin
+
+# flash master steps
+pyluos-bootloader flash -t 2 -b .pio/build/l0_master_bootloader/firmware.bin
