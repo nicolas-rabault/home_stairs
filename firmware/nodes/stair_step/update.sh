ae0011fb73aa52246a1a8bbff24fb99a5485aa76
Nicolas Rabault <nicolas.rabault@luos.io>
2022-10-04 13:22:39 +0200

    Add a bootloader reset command before flashing avoiding heavy routing table.

--- firmware/nodes/stair_step/update.sh
+++ firmware/nodes/stair_step/update.sh
@@ -3,6 +3,7 @@
 # build
 pio run -e l0_bootloader -e l0_master_bootloader
 
+pyluos-bootloader reset
 # flash slave steps
 pyluos-bootloader flash -t 3 4 5 6 7 8 9 10 11 12 -b .pio/build/l0_bootloader/firmware.bin
 
